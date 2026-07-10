264b16b46c3a2c1488374449f2b26fd11ad9fc14
JhawksCI <jhawksci@gmail.com>
2026-07-10 00:55:17 +0000

    Update main.js

--- main.js
+++ main.js
@@ -24,14 +24,14 @@ const games = [
     {
         title: "in progess",
         description: "in progress",
-        image: "assets/images/game2.jpg",
+        image: "assets/images/img2.webp",
         tag: "Plateforme",
         link: "https://jhawks225.itch.io/nom-du-jeu-2"
     },
     {
         title: "in progress",
         description: "in progress",
-        image: "assets/images/game3.jpg",
+        image: "assets/images/img3.webp",
         tag: "Puzzle",
         link: "https://jhawks225.itch.io/nom-du-jeu-3"
     }
